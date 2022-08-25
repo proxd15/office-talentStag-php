54924e263a7e06893222b5d587d64c301bf1f90c
proxd15 <94702040+proxd15@users.noreply.github.com>
2022-08-25 02:07:37 -0800

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,68 @@
+<?php
+$page = 'about';
+include 'head.php';
+?>
+<style>
+    .text-dec {
+        color: #ff771d;
+    }
+    .mtop{
+      margin-top: 100px;
+    }
+</style>
+<section class="about-us" id="about-us">
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-6">
+                <h1 style="color:  #ff771d;">About Us</h1>
+                <hr>
+                <p>"Assist the company in outperforming at every level." is what Talent Stag strives for.</p>
+                <p> Talent Stag, one of the well-known ERP vendors, was established in 2022. It strives to offer
+                    knowledgeable
+                    human resources to every firm and manage regular business operations including payroll
+                    administration,
+                    outsourcing, and background checks. With every service we provide, our aim is to continually meet
+                    clients'
+                    expectations. Our friendly, competent staff is equipped with the training, knowledge, and expertise
+                    to
+                    guarantee that the human resources division of your company operates ethically, effectively, and
+                    strategically. We value the confidence you have in us and are committed to building strong customer
+                    relationships.</p>
+            </div>
+            <div class="col-md-6 mtop">
+                <img src="http://themebubble.com/demo/marketingpro/wp-content/uploads/2016/10/seo-slide.png " alt="">
+            </div>
+        </div>
+        <div class=" my-4">
+            <h2 class="text-dec">Mission</h2>
+            <div style="text-align: justify;">
+                Our goal is to help the business overcome the obstacles that prevent it from functioning effectively so
+                that
+                it can achieve its objectives.<br><br>
+                We empower businesses by recruiting talented individuals based on their ability and skill within the
+                constraints of the company's budget and role.<br><br>
+                Under our payroll management solution, we strive to provide businesses with flexibility, speed, and
+                accuracy
+                through advanced technology and automation.<br><br>
+                In order for our customers to operate smoothly over the long term, we work to provide them with security
+                regarding their employees with fully verified background checks.<br><br>
+                We stay true to the people, connecting them with one another and assisting them in outsourcing to their
+                desired individuals.
+
+            </div>
+        </div>
+        <div class="my-4">
+            <h2 class="text-dec">Vision</h2>
+            <div>
+                The goal of Talent Stag is to assist businesses in streamlining their operations and processes while
+                maintaining the greatest levels of production and efficiency. Our vision is to remain innovative and
+                updated
+                in order to be recognized as one of the leading firms in the market in which we operate.
+
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include 'footer.php';
+?>
